7e55185e648f0517e9b196ba2dcb73d7b5cae060
nurzat04 <nurzat03.10q@gmail.com>
2025-12-19 10:12:35 +0600

    updated

--- back/index.js
+++ back/index.js
@@ -15,7 +15,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://recipe-sharing-platform-ochre-zeta.vercel.app",
+
     credentials: true,
 }));
 app.use(express.json());
@@ -33,10 +34,10 @@ app.use('/chefs', chefRoutes);
 
 // Root endpoint
 app.get('/', (req, res) => {
-    res.json({ 
-        message: 'Recipe Sharing Platform API', 
+    res.json({
+        message: 'Recipe Sharing Platform API',
         version: '1.0.0',
-            endpoints: {
+        endpoints: {
             health: '/health',
             users: '/users',
             recipes: '/recipes',
